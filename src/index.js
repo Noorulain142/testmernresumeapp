03001f9fcb496f1f7929dee55b17bde0d05a77fa
Noor Ul Ain <noor.ulain@devsinc.com>
2022-12-26 22:24:31 -0800

    new commit

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client'));
 }
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("App is running on port " + port);
 });
